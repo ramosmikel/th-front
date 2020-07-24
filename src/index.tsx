3f5f72ae6a31dea2eb1d52a471cd72311ac4e84e
Mikel Ramos <ramosmikel@icloud.com>
2020-07-24 18:30:24 +0100

    53#

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Listings } from "./sections";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <Listings title="TinyHouse Listing" />,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/sections/Listings/Listings.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import { server } from "../../lib/api";
+import {
+  DeleteListingData,
+  DeleteListingVariables,
+  Listing,
+  ListingsData,
+} from "./types";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
+
+const DELETE_LISTING = `
+  mutation DeleteListing($id: ID!) {
+    deleteListing(id: $id) {
+      id
+    }
+  }
+`;
+
+interface Props {
+  title: string;
+}
+
+export const Listings = ({ title }: Props) => {
+  const [listings, setListings] = useState<Listing[] | null>(null);
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    setListings(data.listings);
+  };
+
+  const deleteListing = async (id: string) => {
+    await server.fetch<DeleteListingData, DeleteListingVariables>({
+      query: DELETE_LISTING,
+      variables: {
+        id,
+      },
+    });
+    fetchListings();
+  };
+
+  const listingList = listings ? (
+    <ul>
+      {listings.map((listing) => {
+        return (
+          <li key={listing.id}>
+            {listing.title}
+            <button onClick={() => deleteListing(listing.id)}>Delete</button>
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      {listingList}
+    </div>
+  );
+};
