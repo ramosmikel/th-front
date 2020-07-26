f45c122171079bc7143f1338f7fd81cca30138a0
Mikel Ramos <ramosmikel@icloud.com>
2020-07-26 20:31:45 +0100

    #89 Executing Login

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ApolloClient from "apollo-boost";
@@ -13,14 +13,24 @@ import {
   NotFound,
   User,
 } from "./sections";
+import { Viewer } from "./lib/types";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 
 const client = new ApolloClient({
   uri: "/api",
 });
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
 
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+
   return (
     <Router>
       <Layout id="app">
@@ -29,7 +39,11 @@ const App = () => {
           <Route exact path="/host" component={Host} />
           <Route exact path="/listing:id" component={Listing} />
           <Route exact path="/listings/:location?" component={Listings} />
-          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
           <Route exact path="/user/:id" component={User} />
           <Route component={NotFound} />
         </Switch>
--- src/sections/Login/index.tsx
+++ src/sections/Login/index.tsx
@@ -1,15 +1,88 @@
-import React from "react";
-import { Layout, Card, Typography } from "antd";
+import React, { useEffect, useRef } from "react";
+import { Redirect } from "react-router";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import { Card, Layout, Spin, Typography } from "antd";
+import { ErrorBanner } from "../../lib/components";
+import { AUTH_URL } from "../../lib/graphql/queries";
+import { LOG_IN } from "../../lib/graphql/mutations";
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from "../../lib/graphql/mutations/LogIn/__generated__/LogIn";
+import { AuthUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl";
+import {
+  displayErrorMessage,
+  displaySuccessNotification,
+} from "../../lib/utils";
+import { Viewer } from "../../lib/types";
 
 // Image Assets
 import googleLogo from "./assets/google_logo.jpg";
 
-const { Content } = Layout;
-const { Text, Title } = Typography;
+interface Props {
+  setViewer: (viewer: Viewer) => void;
+}
 
-export const Login = () => {
+export const Login = ({ setViewer }: Props) => {
+  const client = useApolloClient();
+  const [
+    logIn,
+    { data: logInData, loading: logInLoading, error: logInError },
+  ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn && data.logIn.token) {
+        setViewer(data.logIn);
+        displaySuccessNotification("You've successfully logged in!");
+      }
+    },
+  });
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    if (code) {
+      logInRef.current({
+        variables: {
+          input: { code },
+        },
+      });
+    }
+  }, []);
+
+  const { Content } = Layout;
+  const { Text, Title } = Typography;
+
+  const handleAuthorize = async () => {
+    try {
+      const { data } = await client.query<AuthUrlData>({ query: AUTH_URL });
+      window.location.href = data.authUrl;
+    } catch {
+      displayErrorMessage(
+        "Sorry! We weren't able to log you in. Please try again later!"
+      );
+    }
+  };
+
+  if (logInLoading) {
+    return (
+      <Content className="log-in">
+        <Spin size="large" tip="Logging you in...." />
+      </Content>
+    );
+  }
+
+  if (logInData && logInData.logIn) {
+    const { id: viewerId } = logInData.logIn;
+    return <Redirect to={`/user/${viewerId}`} />;
+  }
+
+  const logInErrorBannerElement = logInError ? (
+    <ErrorBanner description="Sorry! We weren't able to log you in. Please try again later!" />
+  ) : null;
+
   return (
     <Content className="log-in">
+      {logInErrorBannerElement}
       <Card className="log-in-card">
         <div className="log-in-card__intro">
           <Title level={3} className="log-in-card__intro-title">
@@ -18,11 +91,14 @@ export const Login = () => {
             </span>
           </Title>
           <Title level={3} className="log-in-card__intro-title">
-            Log in to TinyHouse!
+            Log in to Recat!
           </Title>
           <Text>Sign in with Google to start booking available rentals!</Text>
         </div>
-        <button className="log-in-card__google-button">
+        <button
+          className="log-in-card__google-button"
+          onClick={handleAuthorize}
+        >
           <img
             src={googleLogo}
             alt="Google Logo"
@@ -33,8 +109,8 @@ export const Login = () => {
           </span>
         </button>
         <Text type="secondary">
-          Note: By signing in, you'll be redirected to the Google consent form
-          to sign in with your Google account.
+          Note: By signing in, you'll be redirected to Google's consent form to
+          sign in with your Google account.
         </Text>
       </Card>
     </Content>
